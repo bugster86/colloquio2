8fadac141a6879f3a5d5bfb99f48e4b402013985
Martino Viganò <martino.vigano@staff.aruba.it>
2024-12-18 11:27:53 +0100

    versione 1.1

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # coding=utf-8
 # Autore: Martino Vigano'
-# Versione: 1.0
+# Versione: 1.1
 # Aggiornamenti
+# 1.1 --> Esteso a 401 il ciclo
 
 for numero in range(1,401):
 	if  numero%5 ==0 and numero%3 ==0:
